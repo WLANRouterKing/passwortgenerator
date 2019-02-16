6c17c9083a8288d285c07b01e2496fdac7a9be56
LXkennstenich <kontakt@alexanderweese-webdesign.de>
2019-02-16 15:41:20 +0100

    Repository erstellt

---
+++ PasswortGenerator/Classes/Settings.cs
@@ -0,0 +1,212 @@
+﻿namespace PasswortGenerator.Classes
+{
+    class Settings
+    {
+        /// <summary>
+        /// use lowercase ?
+        /// </summary>
+        private bool UseLowercase { get;  set; }
+
+        /// <summary>
+        /// use uppercase ?
+        /// </summary>
+        private bool UseUppercase { get;  set; }
+
+        /// <summary>
+        /// use numbers ?
+        /// </summary>
+        private bool UseNumbers { get;  set; }
+
+        /// <summary>
+        /// use specialchars ?
+        /// </summary>
+        private bool UseSpecialchars { get;  set; }
+
+        /// <summary>
+        /// the default password length for generating passwords
+        /// </summary>
+        private int DefaultPasswordlength { get;  set; }
+
+        /// <summary>
+        /// allowed special characters for the password
+        /// </summary>
+        private string AllowedSpecialchars { get;  set; }
+
+        /// <summary>
+        /// the url from where updates getting pulled
+        /// </summary>
+        private string UpdateUrl { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Settings()
+        {
+            SetUseLowercase(Properties.Settings.Default.UseLowercase);
+            SetUseUppercase(Properties.Settings.Default.UseUppercase);
+            SetUseNumbers(Properties.Settings.Default.UseNumbers);
+            SetUseSpecialchars(Properties.Settings.Default.UseSpecialchars);
+            SetDefaultPasswordlength(Properties.Settings.Default.DefaultPasswordlength);
+            SetAllowedSpecialchars(Properties.Settings.Default.AllowedSpecialchars);
+            SetUpdateUrl(Properties.Settings.Default.UpdateUrl);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        private void SetUpdateUrl(string value)
+        {
+            UpdateUrl = value;
+        }
+
+        /// <summary>
+        /// Setter Lowercase Setting
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetUseLowercase(bool value)
+        {
+            UseLowercase = value;
+        }
+
+        /// <summary>
+        /// Setter Uppercase Setting
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetUseUppercase(bool value)
+        {
+            UseUppercase = value;
+        }
+
+
+        /// <summary>
+        /// Setter Number Setting
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetUseNumbers(bool value)
+        {
+            UseNumbers = value;
+        }
+
+        /// <summary>
+        /// Setter Specialchars Setting
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetUseSpecialchars(bool value)
+        {
+            UseSpecialchars = value;
+        }
+
+        /// <summary>
+        /// Setter Defaultpasswordlength Setting
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetDefaultPasswordlength(int value)
+        {
+            DefaultPasswordlength = value;
+        }
+
+        /// <summary>
+        /// Setter Allowedspecialchars Setting
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetAllowedSpecialchars(string value)
+        {
+            AllowedSpecialchars = value;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public bool GetUseUppercase()
+        {
+            return UseUppercase;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public bool GetUseNumbers()
+        {
+            return UseNumbers;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public bool GetUseSpecialchars()
+        {
+            return UseSpecialchars;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public int GetDefaultPasswordlength()
+        {
+            return DefaultPasswordlength;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string GetAllowedSpecialchars()
+        {
+            return AllowedSpecialchars;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public bool GetUseLowercase()
+        {
+            return UseLowercase;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string GetUpdateUrl()
+        {
+            return UpdateUrl;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Reset()
+        {
+            Properties.Settings.Default.Reset();
+            Properties.Settings.Default.UseLowercase = true;
+            Properties.Settings.Default.UseUppercase = true;
+            Properties.Settings.Default.UseNumbers = true;
+            Properties.Settings.Default.UseSpecialchars = true;
+            Properties.Settings.Default.DefaultPasswordlength = 16;
+            Properties.Settings.Default.AllowedSpecialchars = @"+-=_@#$%^&;:,.<>/~\[](){}?!|*";
+            Properties.Settings.Default.Save();
+            Properties.Settings.Default.Reload();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Save()
+        {
+            Properties.Settings.Default.UseLowercase = GetUseLowercase();
+            Properties.Settings.Default.UseUppercase = GetUseUppercase();
+            Properties.Settings.Default.UseNumbers = GetUseNumbers();
+            Properties.Settings.Default.UseSpecialchars = GetUseSpecialchars();
+            Properties.Settings.Default.DefaultPasswordlength = GetDefaultPasswordlength();
+            Properties.Settings.Default.AllowedSpecialchars = GetAllowedSpecialchars();
+            Properties.Settings.Default.Save();
+            Properties.Settings.Default.Reload();
+        }
+    }
+}
---
+++ PasswortGenerator/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Windows;
+using AutoUpdaterDotNET;
+using Sodium;
+using PasswortGenerator.Classes;
+
+namespace PasswortGenerator
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        /// <summary>
+        /// Object containing the settings
+        /// </summary>
+        Settings settings;
+
+        /// <summary>
+        /// nothing to say
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            settings = new Settings();
+            Init();
+            AutoUpdater.AppTitle = "Passwort Generator";
+            AutoUpdater.Start(settings.GetUpdateUrl());
+        }
+
+        /// <summary>
+        /// Initialize the settings
+        /// </summary>
+        private void Init()
+        {
+            if (settings.GetUseLowercase() == true)
+            {
+                LowercaseCheckbox.IsChecked = true;
+            }
+
+            if (settings.GetUseUppercase() == true)
+            {
+                UppercaseCheckbox.IsChecked = true;
+            }
+
+            if (settings.GetUseNumbers() == true)
+            {
+                NumberCheckbox.IsChecked = true;
+            }
+
+            if (settings.GetUseSpecialchars() == true)
+            {
+                SpecialcharsCheckbox.IsChecked = true;
+            }
+
+            DefaultPasswordlengthTextbox.Text = Convert.ToString(settings.GetDefaultPasswordlength());
+            AllowedSpecialcharsTextbox.Text = settings.GetAllowedSpecialchars();
+        }
+
+        private void GeneratePasswordButton_Click(object sender, RoutedEventArgs e)
+        {
+            string characters = "";
+            string password = "";
+
+            if(settings.GetUseLowercase() == true)
+            {
+                characters += "abcdefghijklmnopqrstuvwxyz";
+            }
+
+            if(settings.GetUseUppercase() == true)
+            {
+                characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            }
+
+            if(settings.GetUseNumbers() == true)
+            {
+                characters += "0123456789";
+            }
+
+            if(settings.GetUseSpecialchars() == true)
+            {
+                characters += settings.GetAllowedSpecialchars();
+            }
+
+            for(int i = 0; i < settings.GetDefaultPasswordlength(); i++)
+            {
+                int random = SodiumCore.GetRandomNumber(characters.Length);
+                char c = characters[random];
+                password += c;
+            }
+
+            GeneratedPasswordTextbox.Text = password;
+        }
+
+        private void SaveSettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bool uselowercase = LowercaseCheckbox.IsChecked == true ? true : false;
+                bool useuppercase = UppercaseCheckbox.IsChecked == true ? true : false;
+                bool usenumbers = NumberCheckbox.IsChecked == true ? true : false;
+                bool usespecialchars = SpecialcharsCheckbox.IsChecked == true ? true : false;
+                int defaultpasswordlength = Convert.ToInt32(DefaultPasswordlengthTextbox.Text);
+                string allowedspecialchars = AllowedSpecialcharsTextbox.Text;
+
+                settings.SetUseLowercase(uselowercase);
+                settings.SetUseUppercase(useuppercase);
+                settings.SetUseNumbers(usenumbers);
+                settings.SetUseSpecialchars(usespecialchars);
+                settings.SetDefaultPasswordlength(defaultpasswordlength);
+                settings.SetAllowedSpecialchars(allowedspecialchars);
+                settings.Save();
+                SettingsResponseLabel.Content = "Einstellungen gespeichert";
+            }
+            catch(Exception ex)
+            {
+                SettingsResponseLabel.Content = ex.Message;
+            }
+        }
+    }
+}
